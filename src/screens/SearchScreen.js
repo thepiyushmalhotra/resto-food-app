e371a4203c304d1094bfe403abc96a7f1648a180
Piyush Malhotra <piyush@soulber.co>
2022-03-11 12:53:26 -0500

    add searchbar, searchScreen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
+
+const SearchScreen = () => {
+  const [term, setTerm] = useState(""); // useState anytime we need to update content in a screen
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // Await is used to wait for the response from the API. Async keyword is used to make the code asynchronous.
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something went wrong");
+    }
+  };
+
+  return (
+    <View>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
