3406efbacb399e72b034ea9040dc7f059db688d8
Piyush Malhotra <piyush@soulber.co>
2022-03-15 14:01:21 -0400

    render flatlist element

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState(""); // useState anytime we need to update content in a screen
   const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    // price === "$" || "$$" || "$$$" || "$$$$"
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
 
   return (
     <View>
@@ -16,6 +24,13 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice("$")} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice("$$")} title="A Bit Pricier" />
+      <ResultsList results={filterResultsByPrice("$$$")} title="Big Spender" />
+      <ResultsList
+        results={filterResultsByPrice("$$$$")}
+        title="Lavish Lunch"
+      />
     </View>
   );
 };
